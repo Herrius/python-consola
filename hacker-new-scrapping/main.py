9b8b20f3be06291b4c4713388eec02b1ba05a955
Herrius <enriqueubaldo97@gmail.com>
2022-08-11 16:34:59 -0500

    web scrapping hacker news

---
+++ hacker-new-scrapping/main.py
@@ -0,0 +1,21 @@
+from operator import indexOf
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    response= requests.get('https://news.ycombinator.com/')
+    yc_web_page=response.text
+    soup=BeautifulSoup(yc_web_page,'html.parser')
+    array_title=[]
+    array_points=[]
+    title=soup.find_all('a',class_='titlelink')
+    points=soup.find_all('span',class_='score')
+    for text in title:
+        array_title.append(text.getText())
+    for text in points:
+        array_points.append(int(text.getText().split()[0]))
+    max_value=max(array_points)
+    pos_max_value=array_points.index(max_value)
+    max_vote_news=array_title[pos_max_value]
+    print(max_vote_news,max_value)
+main()
